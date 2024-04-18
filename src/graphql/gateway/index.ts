bcaf78c629d4f6548569b63f15958ffafb3c7012
Alexander Zakharov <alexander.zakharov@aldra.no>
2024-04-18 20:33:07 +0200

    fix: do not use enum from '@apollo/gateway' package

--- src/graphql/gateway/index.ts
+++ src/graphql/gateway/index.ts
@@ -3,7 +3,7 @@ import {
   IntrospectAndCompose,
   RemoteGraphQLDataSource,
 } from '@apollo/gateway';
-import { GraphQLDataSourceRequestKind } from '@apollo/gateway/dist/datasources/types';
+import { type GraphQLDataSourceRequestKind } from '@apollo/gateway/dist/datasources/types';
 import { type ApolloServerOptionsWithGateway } from '@apollo/server';
 
 import env from '@project/env';
@@ -23,7 +23,10 @@ export default new ApolloGateway({
     new RemoteGraphQLDataSource<Apollo.Context>({
       url,
       willSendRequest: ({ kind, request: { http }, context }) => {
-        if (kind === GraphQLDataSourceRequestKind.INCOMING_OPERATION) {
+        if (
+          kind ===
+          ('incoming operation' as GraphQLDataSourceRequestKind.INCOMING_OPERATION)
+        ) {
           const { request, authorization } = context;
 
           http?.headers.set('request-id', request.id);
